23f216b9284999ecf12daf643dbb29bc7ae81b59
Matt Cadorette <mcadorette@gmail.com>
2018-10-09 09:11:56 -0400

    Initial commit from Atomist

---
+++ src/main/java/test123/Test123Application.java
@@ -0,0 +1,12 @@
+package test123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test123Application.class, args);
+	}
+}
